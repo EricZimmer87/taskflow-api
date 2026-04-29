6b47f179c05ff6b13ef5022ddff27f945d37222e
Eric Zimmer <ericzimmer87@fastmail.com>
2026-04-29 10:01:33 -0500

    Initial commit

---
+++ TaskFlow.API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskFlow.API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public List<TaskItem> Tasks { get; set; } = new();
+    }
+}
